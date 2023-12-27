1d5fa65caab7b0c4f35a158db2429660494279b0
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-12-27 12:03:31 +0545

    Feat:test2 complete

--- src/components/test/test2/Mobilenav.jsx
+++ src/components/test/test2/Mobilenav.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaTimes } from "react-icons/fa";
+import "./mobilenav.css";
 
 const Mobilenav = () => {
+  const [clickNav, setClickNav] = useState(false);
+
+  const handleClick = () => setClickNav(!clickNav);
   return (
     <>
       <nav className="navbar">
         <div className="nav-container">
           <div className="nav-logo">CoDeR</div>
-          <ul className="nav-menu">
+          <ul className={clickNav ? "nav-menu active" : "nav-menu"}>
             <li>
               <a href="/">Home</a>
             </li>
@@ -22,12 +26,23 @@ const Mobilenav = () => {
               <a href="/">About Us</a>
             </li>
           </ul>
-          <div className="nav-icon">
-            <GiHamburgerMenu />
-            <FaTimes />
+          <div className="nav-icon" onClick={handleClick}>
+            {clickNav ? (
+              <span className="icon">
+                <FaTimes />
+              </span>
+            ) : (
+              <span className="icon">
+                <GiHamburgerMenu />
+              </span>
+            )}
           </div>
         </div>
       </nav>
+      <section className="hero-section">
+        <p>Welcome to</p>
+        <h2>Coding Kingdom</h2>
+      </section>
     </>
   );
 };
